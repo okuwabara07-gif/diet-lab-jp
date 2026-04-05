91bb521091e007733de6bd576a178233163e146e
okuwabara07-gif <o.kuwabara07@gmail.com>
2026-04-05 21:35:39 +0900

    feat: add next-sitemap config

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://diet-lab-jp.vercel.app',
+  generateRobotsTxt: true,
+  changefreq: 'daily',
+  priority: 0.7,
+  sitemapSize: 5000,
+}
